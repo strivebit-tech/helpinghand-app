9ced9bac2d0a51e7050bc94e6243a9532c58068b
Abhishek Sirohi <abhishek@totalcloud.io>
2020-04-17 13:39:46 +0530

    build

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Container, Navbar, NavbarBrand, Button, Nav } from "react-bootstrap";
+import logo from "./assets/helpinghands.png";
+import NavbarCollapse from "react-bootstrap/NavbarCollapse";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar variant="light" bg="white" className="shadow-sm">
+        <NavbarBrand>
+          <div className="d-flex align-items-center">
+            <img src={logo} width="50" />
+            <span className="m-0 ml-3 h3 text-danger font-weight-medium">
+              Helping Hands
+            </span>
+          </div>
+        </NavbarBrand>
+
+        <NavbarCollapse className="ml-auto mr-4">
+          <Nav className={"ml-auto"}>
+            <Button variant="success" className="rounded-pill shadow-none">
+              Ask for help
+            </Button>
+
+            <Button variant="warning" className="rounded-pill ml-3 shadow-none">
+              Help Others
+            </Button>
+          </Nav>
+        </NavbarCollapse>
+      </Navbar>
+    </>
   );
 }
 
